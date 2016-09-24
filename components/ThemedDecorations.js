14baf3eb116f7be8d6205f8b9ebcebf0ba4153c4
Alison <alisonhmullin@gmail.com>
2016-09-24 18:33:26 -0400

    complete props children lab

---
+++ components/ThemedDecorations.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ThemedDecorations extends React.Component {
+  render (){
+    const childrenWithNewProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme,
+      });
+    });
+  return (
+    <div>
+      {childrenWithNewProp}
+    </div>
+    );
+    }
+  }
+
+module.exports = ThemedDecorations;
